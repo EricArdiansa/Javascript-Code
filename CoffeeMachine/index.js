e7b5e21ae1a07affff020936a7e8e16c2bde379e
EricArdi <ericardiansa@gmail.com>
2023-04-06 11:35:39 +0700

    testing File

--- CoffeeMachine/index.js
+++ CoffeeMachine/index.js
@@ -5,3 +5,49 @@ console.log("Mencampurkan air dan kopi");
 console.log("Menuangkan kopi ke dalam gelas");
 console.log("Menuangkan susu ke dalam gelas");
 console.log("Kopi Anda sudah siap!");
+
+const coffeeStock = require("./state");
+
+console.log(coffeeStock);
+
+// Dalam inisialisasi variabel coffeeStock (nama variabel bebas kita tentukan),
+// kita gunakan method require() dengan memberikan parameter lokasi berkas state.js.
+// Dengan begitu variabel coffeeStock akan memiliki nilai module.exports yang sama pada berkas state.js.
+//  Setelah mendapatkan nilainya, kita bebas menggunakannya seperti variabel lokal pada umumnya.
+
+const makeCoffee = (type, miligrams) => {
+  if (coffeeStock[type] >= miligrams) {
+    console.log("Kopi berhasil dibuat!");
+  } else {
+    console.log("Biji kopi habis!");
+  }
+};
+
+makeCoffee("robusta", 80);
+
+/* output
+Kopi berhasil dibuat!
+*/
+//  Jika kita menggunakan lokasi yang relatif (dapat berubah/dipindahkan), pastikan awali dengan menuliskan
+//  ./. Contohnya, berkas index.js dan state.js berada pada folder yang sama, maka kita cukup menuliskannya
+//  dengan ./state.js.
+
+// Lalu bagaimana cara import kedua nilai tersebut? Masih ingat dengan materi destructuring object?
+//  Pada berkas index.js kita gunakan teknik destructuring object untuk mendapatkan nilai yang di-import seperti ini:
+
+const { coffeeStock, isCoffeeMachineReady } = require("./state");
+
+console.log(coffeeStock);
+console.log(isCoffeeMachineReady);
+
+// ES6 Module
+
+// import stock from "./state.js";
+
+// const displayStock = (stock) => {
+//   for (const type in stock) {
+//     console.log(type);
+//   }
+// };
+
+// displayStock(coffeeStock);
--- CoffeeMachine/state.js
+++ CoffeeMachine/state.js
@@ -5,4 +5,19 @@ const coffeeStock = {
 };
 
 module.exports = coffeeStock;
-console.log(module);
+// console.log(module);
+
+// jika Anda mencoba melihat nilai module yang ada pada berkas state.js
+// dengan menambahkan kode console.log(module) di akhir berkas, maka kita
+//  akan melihat object coffeeStock menjadi nilai dari properti exports.
+
+const isCoffeeMachineReady = true;
+
+// Kita tidak bisa melakukan export dari kedua nilai di atas dengan cara seperti di bawah ini
+// module.exports = coffeeStock;
+// module.exports = isCoffeeMachineReady;
+
+// module.exports = { coffeeStock, isCoffeeMachineReady };
+
+// ES6 Module
+export default coffeeStock;
